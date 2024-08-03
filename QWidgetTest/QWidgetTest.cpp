1a8c49704441fe0a0d14830f386d710be6ecca91
soldier <1870056213@qq.com>
2024-08-03 17:24:41 +0800

    update

--- QWidgetTest/QWidgetTest.cpp
+++ QWidgetTest/QWidgetTest.cpp
@@ -1,15 +1,22 @@
 #include "QWidgetTest.h"
 #include "battery/battery.h"
-
+#include <QVBoxLayout>
 QWidgetTest::QWidgetTest(QWidget *parent)
     : BaseWindow<QWidget>(parent)
 {
-    setFixedSize(100, 50);
+    setFixedSize(400, 300);
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->setContentsMargins(0, 0, 0, 0);
+    this->setLayout(layout);
 
     Battery* b = new  Battery(this);
     b->setFixedSize(100, 50);
-
     b->setValue(10);
+
+    layout->addWidget(CreateTitleBar(":public/title_logo.ico", QString::fromStdWString(L"²âÊÔ³ÌÐò")));
+    layout->addStretch();
+    layout->addWidget(b);
 }
 
 QWidgetTest::~QWidgetTest()
